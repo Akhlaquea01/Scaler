c927a9e654178fad354c731cc3b86a76abf2ca34
Akhlaque <akhlaquea01@gmail.com>
2022-10-24 00:15:58 +0530

    Begganers Module Completed

---
+++ JavaBasics/002-Intro-to-Java/intro_to_java.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.lang.*;	
+
+class Main{
+	
+	public static void main(String args[]){
+		// Write your code here
+		
+		// IDE -> Integrated Development Environment
+		// Area where you can code		
+		
+		// Syntax
+		/*
+			There is a dog on the road. -> Correct
+			A dog on road the is there. -> Incorrect grammar
+		
+			Java -> Programming language
+			Programming language grammar/rules -> Syntax
+		*/
+		
+		// System.out.print("Hello World!"); // Correct
+		// system.out.print("Hello World!"); // Incorrect
+		// System.out("Hello World!"); // Incorrect
+		// System.out.print(Hello world!); Incorrect
+		
+		// System.out.print("Hello Aarnav!"); // Text - use ""
+		// System.out.print(123); // Numbers - don't use ""
+		// System.out.print(12.56);
+		// System.out.print(-10.23);
+		
+		// System.out.print("123");
+		// System.out.print("Hello world 23");
+		
+		// Prints it joined together in output
+		// System.out.print("Hello");
+		// System.out.print("World");
+		// System.out.print(18);
+		
+		// Prints with a space in output
+		// System.out.print("Hello ");
+		// System.out.print("World!");
+		
+		// Printing in a new line
+		// 	System.out.println("Hello");
+		// 	System.out.print("World!");
+		// 	System.out.print(123);
+		
+		// Comments
+		// -> Single line comment
+		/*
+		   -> Multi line comment
+		*/
+		
+		// Basic maths in output
+		// System.out.println(12+3);
+		// System.out.println(12-3);
+		// System.out.println(12*3);
+		// System.out.println(12/3);
+		
+		// Print numbers and text together
+		// System.out.println("Hello " + "World!");
+		// System.out.println("Hello" + 18);
+		// System.out.println(18 + "Hello");
+	}
+}
---
+++ JavaBasics/003-input-&-output/input_&_output.java
@@ -0,0 +1,121 @@
+import java.util.*;
+class Main {
+	public static void main(String args[]) {
+		// Write your code here
+		// IDE - Integerated Development Environment
+		// System.out.print("Hello world !");
+		
+		// Instructions -> User interaction ( Input ) -> 
+		// Computer -> Result ( Output )
+		// System.out.print(10);
+		// System.out.print(20.59345793);
+		// System.out.print("Hello my name is Aarnav Jindal."); // -> Enligh . || Java ;
+		
+		// Comments in Java -> // ( Rough Work )
+		// Single line comment 
+		
+		// Multi-line comment
+		/*
+		System.out.print(10);
+		System.out.print(10);
+		System.out.print(10);
+		System.out.print(10);
+		*/
+		
+		// System.out.print("Hello World!"); // English, Syntax
+		// System.out.print(10); // Numbers, Syntax
+		
+		// Case-sensitive
+		// System.out.print("5+10");
+		// System.out.print("20+3");
+		
+		// Predict the output
+		// system.out.print(5+10)
+		// 15 -> Wrong
+		// 5+10 -> Wrong
+		// Error -> Correct
+		
+		// System.out.print("Hello" + "World"); 
+		// HelloWorld -> Concatenation
+		
+		// System.out.print("Hello " + "World");
+		// Hello World
+		
+		// System.out.print("Hello"+10);
+		// Hello10
+		
+		// System.out.print("Hello "+10);
+		
+		// System.out.println("Name : Aarnav");
+		// System.out.print("Age : 50");
+		
+		// System.out.print("10");
+		// 10
+		// "10"
+		
+		// "Hello" + " " + 10 -> "Hello "+10 -> "Hello 10"
+		
+		// Break -> 10:35
+		
+		// This is Refresher Batch
+		// To go to beginner batch email at support@scaler.com
+		
+		// Use ide.new for coding
+		
+		// System.out.println(10+20+"Hello");
+		// 10 + 20 -> 30
+		// 30 + Hello
+		// 30Hello
+		// System.out.println("Hello"+10+20);
+		// "Hello" + 10
+		// "Hello10" + 20
+		// "Hello1020"
+		
+		// int x;
+		// x = 10;
+		// // int x = 10;
+		// System.out.print(x);
+		
+		// double y;
+		// y = 1.58;
+		// System.out.print(y);
+		
+		// float f;
+		// f = 1.4f;
+		// System.out.print(f);
+		
+		// long l;
+		// l = 1000000000000l;
+		// System.out.print(l);
+		
+		// Scanner is used to take input from user
+		Scanner scn = new Scanner(System.in);
+		// int x;
+		// Input 
+		// x = scn.nextInt();
+		// Otutput
+		// System.out.print(x);
+		
+		// long y;
+		// y = scn.nextLong();
+		// System.out.print(y);
+		
+		// float v;
+		// v = scn.nextFloat();
+		// System.out.print(v);
+		
+		// double z;
+		// z = scn.nextDouble();
+		// System.out.print(z);
+		
+		// int x;
+		// float y;
+		// x = scn.nextInt();
+		// y = scn.nextFloat();
+		// System.out.println(x);
+		// System.out.println(y);
+		
+		int x;
+		long x;
+	}
+}
